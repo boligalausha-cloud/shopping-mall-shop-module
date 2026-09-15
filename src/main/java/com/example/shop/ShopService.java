5ef7958360bf0bab6c62ef36fc711ed4909dc7ed
boligalausha-cloud <boligalausha@gmail.com>
2026-09-15 21:48:49 +0530

    Add Spring annotations to ShopService

--- src/main/java/com/example/shop/ShopService.java
+++ src/main/java/com/example/shop/ShopService.java
@@ -1,7 +1,8 @@
 package com.example.shop;
 
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 @Service
 public class ShopService {
     @Autowired ShopRepository repo;
